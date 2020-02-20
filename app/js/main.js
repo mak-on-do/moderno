f8b2d1f10eda58994a2d17ca04e1a4515b2753d8
mak-on-do <tohafedor@gmail.com>
2020-02-20 16:23:20 +0400

    added products-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
 $(function() {
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $('.products-slider__inner').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 
     var mixer = mixitup('.products__inner-box');
 
